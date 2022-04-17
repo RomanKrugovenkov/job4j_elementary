a22a4732bc2b1df03a44fe9489acc69713db0221
krugo <krugovenkov@mail.ru>
2022-04-17 18:24:03 +0300

    5.0.1. Сумма чисел. [#226416]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        int i = 0;
-        for (i = start; i <= finish; i++) {
-            sum = sum + i;
+        for (start = start; start <= finish; start++) {
+            sum = sum + start;
         }
         return sum;
     }
